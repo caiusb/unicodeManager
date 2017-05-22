5546ce28d18ef84b7f26979dba68a30de80bb90c
Caius Brindescu <brindesc@oregonstate.edu>
2017-05-22 14:44:30 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='UnicodeManager',
+    version="0.1",
+    description="Wrapper for the Python csv module that handles UTF-8 correctly",
+    packages=['unicodeManager']
+)
