4238d7e1b13744494441e3041b1286a547941e05
Caius Brindescu <brindesc@oregonstate.edu>
2017-05-22 15:06:10 -0700

    Spelled with a lowercase u

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='UnicodeManager',
+setup(name='unicodeManager',
     version="0.1",
     description="Wrapper for the Python csv module that handles UTF-8 correctly",
     packages=['unicodeManager']
